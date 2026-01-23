37335a8a2404ed21d319f49a640b3d2645e67dac
Fred Clamp-Gray <fredcg@Freds-MacBook-Pro.local>
2026-01-23 14:31:44 +0000

    initial proj structure

---
+++ calibrate.cpp
@@ -0,0 +1,13 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <string>
+#include <iostream>
+#include <tuple>
+#include <time.h>
+
+
+int main(int argc, char* argv[]){
+
+    return 0;
+}
+
